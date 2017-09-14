205e63b1a030432d875bffdde0acfe7013d1c900
John Bywater <john.bywater@appropriatesoftware.net>
2017-09-14 14:09:38 +0100

    Removed all modes of evaluating a dependency graph that involve moving numpy arrays across a process boundary.

--- quantdsl/application/with_greenthreads_and_python_objects.py
+++
@@ -1,7 +0,0 @@
-from quantdsl.application.with_greenthreads import QuantDslApplicationWithGreenThreads
-from quantdsl.application.with_pythonobjects import QuantDslApplicationWithPythonObjects
-
-
-class QuantDslApplicationWithGreenThreadsAndPythonObjects(QuantDslApplicationWithGreenThreads,
-                                                          QuantDslApplicationWithPythonObjects):
-    pass
--- quantdsl/application/with_multiprocessing_and_cassandra.py
+++
@@ -1,9 +0,0 @@
-from quantdsl.application.with_cassandra import QuantDslApplicationWithCassandra
-from quantdsl.application.with_multiprocessing import QuantDslApplicationWithMultiprocessing
-
-
-class QuantDslApplicationWithMultiprocessingAndCassandra(QuantDslApplicationWithMultiprocessing,
-                                                         QuantDslApplicationWithCassandra):
-
-    pass
-
