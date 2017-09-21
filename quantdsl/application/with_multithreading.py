bcb745aaa0bafb91e567cf9778a45a903331d346
John Bywater <john.bywater@appropriatesoftware.net>
2017-09-21 19:25:35 +0100

    Catch TimeoutError in worker threads looping on evaluation queue.

--- quantdsl/application/with_multithreading.py
+++ quantdsl/application/with_multithreading.py
@@ -2,6 +2,7 @@ from threading import Thread
 import six.moves.queue as queue
 
 from quantdsl.application.base import QuantDslApplication
+from quantdsl.exceptions import TimeoutError
 
 
 class QuantDslApplicationWithMultithreading(QuantDslApplication):
@@ -11,6 +12,13 @@ class QuantDslApplicationWithMultithreading(QuantDslApplication):
                                                                     *args, **kwargs)
         # Start evaluation worker threads.
         for _ in range(num_threads):
-            t = Thread(target=self.loop_on_evaluation_queue)
+            t = Thread(target=self.protected_loop_on_evaluation_queue)
             t.setDaemon(True)
             t.start()
+
+    def protected_loop_on_evaluation_queue(self):
+        try:
+            self.loop_on_evaluation_queue()
+        except TimeoutError:
+            # Don't need to see unhandled timeout exceptions from worker threads.
+            pass
