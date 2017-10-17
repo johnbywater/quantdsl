32beb6b524c081a8041429ed39880f289157433d
John Bywater <john.bywater@appropriatesoftware.net>
2017-10-17 15:37:25 +0100

    Removed unused line.

--- quantdsl/domain/services/call_links.py
+++ quantdsl/domain/services/call_links.py
@@ -3,8 +3,6 @@ def regenerate_execution_order(contract_specification_id, call_link_repo):
     link_id = contract_specification_id
     while True:
         call_id = get_next_call_id(call_link_repo, link_id)
-        if call_id is None:
-            break
         yield call_id
         if call_id == contract_specification_id:
             break
