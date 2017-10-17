573c3bdd4adc332ef5d1746f882a8c387f287815
John Bywater <john.bywater@appropriatesoftware.net>
2017-10-17 15:32:03 +0100

    Removed unused code.

--- quantdsl/domain/model/call_specification.py
+++
@@ -1,7 +0,0 @@
-## Domain model objects.
-from collections import namedtuple
-
-
-CallSpecification = namedtuple('CallSpecification', ['id', 'dsl_expr_str',
-                                                     'evaluation_kwds', 'dependency_values'])
-
