21df9c973ce05b213c11173d85d2ec24e885f0d7
John Bywater <john.bywater@appropriatesoftware.net>
2017-10-23 16:37:09 +0100

    Relaxed version of dependency on six.

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'scipy',
         'python-dateutil==2.2',
         'requests',
-        'six==1.7.3',
+        'six',
         'eventsourcing==0.9.4',
         'pytz',
         'blist',
